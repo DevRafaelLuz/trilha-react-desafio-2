111d0903ab337331b474195f47730edb754e397d
DevRafaelLuz <rafael.luz2005@gmail.com>
2024-10-30 09:13:52 -0300

    Remoção do repositório pelo id desenvolvida

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
@@ -9,34 +8,27 @@ import { api } from '../services/api';
 import { Container } from './styles';
 
 function App() {
-
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
+    const {data} = await api.get(`repos/${currentRepo}`);
 
-    const {data} = await api.get(`repos/${currentRepo}`)
-
-    if(data.id){
-
+    if (data.id) {
       const isExist = repos.find(repo => repo.id === data.id);
 
-      if(!isExist){
+      if (!isExist) {
         setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        setCurrentRepo('');
+        return;
       }
-
     }
-    alert('Repositório não encontrado')
-
+    alert('Repositório não encontrado');
   }
 
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(prevRepos => prevRepos.filter(repo => repo.id !== id));
   }
 
 
